b13549824b64e350428db3153f2a49f40aead86b
pablito <3387@holbertonschool.com>
2021-11-09 11:24:10 -0500

    I added a function that display the complex number

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * display_complex_number - display the complex number.
+ *
+ * @c: complex structure
+ */
+void display_complex_number(complex c)
+{
+	if (c.re == 0 && c.im == 0)
+		printf("%d", 0);
+	else
+	{
+		if (c.re == 0 && c.im != 0)
+			printf("%d", 0);
+		if (c.re != 0)
+			printf("%g", c.re);
+		if (c.im > 0)
+			printf(" + ");
+		else if (c.im < 0)
+			printf(" - ");
+		if (c.im > 0 && c.im != 1)
+			printf("%gi", c.im);
+		else if (c.im == 1)
+			printf("i");
+		else if (c.im < 0 && c.re != 0 && c.im != -1)
+			printf("%gi", c.im * -1);
+		else if (c.im < 0 && c.re != 0 && c.im == -1)
+			printf("i");
+		else if (c.im < 0 && c.re == 0 && c.im != -1)
+			printf("%gi", c.im);
+		else if (c.im < 0 && c.re == 0 && c.im == -1)
+			printf("i");
+	}
+	printf("\n");
+}
